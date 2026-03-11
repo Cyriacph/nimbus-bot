d8421bdae1bd4084480974d8609b05a372e6798b
Allan <altonallan47@gmail.com>
2026-03-11 13:16:53 +0300

    chore: Hook the !upSkill to bot & commands

--- src/constants/commands.js
+++ src/constants/commands.js
@@ -1,32 +1,33 @@
 // Command-related constants
 
 const COMMANDS = {
-  ROLL: '!roll',
-  SEARCH: '!search',
-  FETCH: '!fetch',
-  ADD: '!add',
-  ADD_HP: '!addhp',
-  SKILLCHECK: '!skillcheck',
-  READ: '!read',
-  STORE: '!store',
-  SET_STATS: '!setstats',
-  MAX_HP: '!maxhp'
+  ROLL: "!roll",
+  SEARCH: "!search",
+  FETCH: "!fetch",
+  ADD: "!add",
+  ADD_HP: "!addhp",
+  SKILLCHECK: "!skillcheck",
+  READ: "!read",
+  STORE: "!store",
+  SET_STATS: "!setstats",
+  MAX_HP: "!maxhp",
+  UPSKILL: "!upskill",
 };
 
 const USAGE = {
-  ROLL: 'Please provide dice notation, e.g., !roll 2d6+3',
-  SEARCH: 'Please provide a keyword to search for, e.g., !search stealth',
-  FETCH: 'Usage: !fetch <characterName>',
-  ADD: 'Usage: !add <characterName> (attach JSON file)',
-  ADD_HP: 'Usage: !addhp <characterName> <value>',
-  SKILLCHECK: 'Usage: !skillcheck <characterName> <skillName>',
-  READ: 'Usage: !read <characterName>',
-  STORE: 'Usage: !store <characterName> <characterSheetJSON>',
-  SET_STATS: 'Usage: !setstats <characterName> <stat> <value>',
-  MAX_HP: 'Usage: !maxhp <characterName> <value>'
+  ROLL: "Please provide dice notation, e.g., !roll 2d6+3",
+  SEARCH: "Please provide a keyword to search for, e.g., !search stealth",
+  FETCH: "Usage: !fetch <characterName>",
+  ADD: "Usage: !add <characterName> (attach JSON file)",
+  ADD_HP: "Usage: !addhp <characterName> <value>",
+  SKILLCHECK: "Usage: !skillcheck <characterName> <skillName>",
+  READ: "Usage: !read <characterName>",
+  STORE: "Usage: !store <characterName> <characterSheetJSON>",
+  SET_STATS: "Usage: !setstats <characterName> <stat> <value>",
+  MAX_HP: "Usage: !maxhp <characterName> <value>",
 };
 
 module.exports = {
   COMMANDS,
-  USAGE
+  USAGE,
 };
